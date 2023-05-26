9fa405f23bdcb239139b627c91f69ef918d756a2
Serhii Ozeran <ozeransm@gmail.com>
2023-05-26 21:36:35 +0300

    update task, add Notiflix,getBoundingClientRect

--- src/index.js
+++ src/index.js
@@ -1,57 +1,109 @@
 import CreateResponce from './js/createResponce'
-import debounce from 'lodash.debounce';
-import simpleLightbox from 'simplelightbox';
-import '../node_modules/simplelightbox/dist/simple-lightbox.min.css'
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from 'notiflix';
+let gallery = null;
+
 const refs ={
     form: document.querySelector('.search-form'),
     galery: document.querySelector('.gallery'),
 }
 refs.form.addEventListener('click',handlerInput);
+refs.form.addEventListener('focusin',handlerClear);
 refs.galery.addEventListener('click', handlerLink);
+
+const data = new CreateResponce();
+
+const options = {
+    root: null,
+    rootMargin: "300px",
+   
+ };
+const target = document.querySelector('.target');
+const observer = new IntersectionObserver(callback, options);
+
+function optiScroll(){
+const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 1.2,
+  behavior: "smooth",
+});
+}
+
+function callback(entr,obs){
+
+entr.forEach(element => {
+    if (element.isIntersecting){
+         data.pageResponce()
+        .then(markUpGalery)
+        .catch(console.log);
+        
+    }
+    });
+}
+
+function handlerClear(e){
+    
+    if((e.target.nodeName==='INPUT')){
+        e.currentTarget.reset();
+        data.clearPage();
+        data.sitePage='';
+        observer.unobserve(target);
+        e.currentTarget.elements[1].removeAttribute('disabled');
+        
+    }
+   
+}
 
 function handlerLink(e){
     e.preventDefault();
-    let gallery = new simpleLightbox('.gallery a');
-    gallery.on('show.simplelightbox', function () {
-	// do something…
-});
 }
 
 function handlerInput(e){
     e.preventDefault();
     if((e.target.nodeName==='BUTTON')&&(e.currentTarget.elements[0].value.trim()!=='')){
-        const data = new CreateResponce(e.currentTarget.elements[0].value);
-        observePage(data);
-    }   
+        refs.galery.textContent='';
+        data.sitePage = e.currentTarget.elements[0].value;
+        observer.observe(target);
+        e.target.setAttribute('disabled',true);
+    } else {refs.galery.textContent=''; if(e.target.nodeName==='BUTTON'){Notiflix.Notify.warning('Empty line. Please try again.')};}
 }
 
 
-function markUpGalery(data){
-    const markUpHtml = data.data.hits.reduce((akk, {webformatURL, largeImageURL, tags})=>akk + `<a href="${largeImageURL}"><img src="${webformatURL}" width="250" height="200" alt="${tags}" title="${tags}"/></a>`,'');
+function markUpGalery(obj){
+    if(!obj){return;}
+    if((data.sitePage*40)>=obj.data.totalHits){observer.unobserve(target); Notiflix.Notify.info("We're sorry, but you've reached the end of search results."); return;}
+    const markUpHtml = obj.data.hits.reduce((akk, {webformatURL, largeImageURL, tags, likes, views, comments, downloads})=>
+    akk + `<div class="photo-card">
+    <a href="${largeImageURL}" >
+    <img src="${webformatURL}" alt="${tags}" width="250" height="200" loading="lazy" title="${tags}"/>
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        <span>${likes}</span>
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        <span>${views}</span>
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        <span>${comments}</span>
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        <span>${downloads}</span>
+      </p>
+    </div>
+    </div>`
+       
+    ,'');
     refs.galery.insertAdjacentHTML('beforeend', markUpHtml);
-   
-}
-
-function observePage(data){
-const options = {
-    root: document.querySelector("#scrollArea"),
-    rootMargin: "300px",
-   
-  };
-
-const target = document.querySelector('.target');
-const observer = new IntersectionObserver(callback, options);
-observer.observe(target);
-
-  function callback(entr,obs){
+    if(data.sitePage===1){gallery = new SimpleLightbox('.gallery a')}else gallery.refresh();
+    optiScroll();
     
-        entr.forEach(element => {
-            if (element.isIntersecting){
-                console.log();
-                data.pageResponce
-                .then(markUpGalery)
-                .catch(console.log);
-            }
-        });
-  }
 }
--- src/js/createResponce.js
+++ src/js/createResponce.js
@@ -1,23 +1,36 @@
 import axios from 'axios';
-
+import Notiflix from 'notiflix';
 
 export default class CreateResponce{
     
-    constructor(value){
+    constructor(){
         this.page=0;
-        this.value=value;
+        this.value='';
+        
     }
     incPage(step=1){
         this.page+=step;
+        
     }
-    get pageResponce(){
+    clearPage(){
+        this.page=0;
+    }
+    async pageResponce()  {
+     if(this.value){ 
         this.incPage();
         const BASE_URL = `https://pixabay.com/api/?key=36684686-da46a32da1515f18ebef67ac3`;
-        return axios.get(`${BASE_URL}&q=${this.value}&page=${this.page}&per_page=40&image_type='photo'&orientation='horizontal'&safesearch='true'`);
-            
+        const result = await axios.get(`${BASE_URL}&q=${this.value}&page=${this.page}&per_page=40&image_type='photo'&orientation='horizontal'&safesearch='true'`);
+        if(result.data.totalHits===0){Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'); return;}
+        else {if(this.page===1){Notiflix.Notify.success(`Hooray! We found totalHits: ${result.data.totalHits} images.`);} return result};
+     }      
     }
 
-    set pageResponce(step=1){
-        this.incPage(step);
+    get sitePage(){
+        return this.page;            
+    }
+
+    set sitePage(value=''){
+        this.value=value;
+        
     }
 }
